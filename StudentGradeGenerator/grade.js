67839bbdd1a231cb82585f21a3375787aec4e50d
Blessings <blessings.marai@student.moringaschool.com>
2023-09-23 17:24:03 +0300

    code written

--- StudentGradeGenerator/grade.js
+++ StudentGradeGenerator/grade.js
@@ -1,13 +1,15 @@
 function calculateGrade(marks) {
-    if (marks > 79) {
-      return 'A';
-    } else if (marks >= 60) {
-      return 'B';
-    } else if (marks >= 50) {
-      return 'C';
-    } else if (marks >= 40) {
-      return 'D';
-    } else {
-      return 'E';
-    }
+  if (marks > 79 && marks <= 100) {
+    return 'A';
+  } else if (marks >= 60 && marks <= 79) {
+    return 'B';
+  } else if (marks >= 50 && marks <= 59) {
+    return 'C';
+  } else if (marks >= 40 && marks <= 49) {
+    return 'D';
+  } else if (marks >= 0 && marks < 40) {
+    return 'E';
+  } else {
+    return 'Invalid input. Marks should be between 0 and 100.';
   }
+}
